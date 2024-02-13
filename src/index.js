1a53e9f57954fed69aa3c771076f9a53bd75fd51
manoty <kevinnoty21@gmail.com>
2024-02-13 11:54:40 +0300

    Comppleted working on events

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<DevSupport ComponentPreviews={ComponentPreviews}
+                            useInitialHook={useInitial}
+>
+    <App/>
+</DevSupport>, document.getElementById('root'));
